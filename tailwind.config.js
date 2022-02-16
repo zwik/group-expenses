fa0b106e78f33ef1bf0ea5081ceb205d8ac5c224
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2022-02-16 10:24:28 +0100

    Upgraded to Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
-};
+}
